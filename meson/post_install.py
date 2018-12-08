5adef465991d3476fa2e795b1dc464538c1af698
Ian M. Jones <ian@ianmjones.com>
2018-12-08 00:58:40 +0000

    Reverted meson/post_install.py as it seems to cause issues with Houston.

--- meson/post_install.py
+++ meson/post_install.py
@@ -4,11 +4,7 @@ import os
 import subprocess
 
 schemadir = os.path.join(os.environ['MESON_INSTALL_PREFIX'], 'share', 'glib-2.0', 'schemas')
-iconcachedir = os.path.join(os.environ['MESON_INSTALL_PREFIX'], 'share', 'icons', 'hicolor')
 
 if not os.environ.get('DESTDIR'):
     print('Compiling gsettings schemas...')
     subprocess.call(['glib-compile-schemas', schemadir])
-
-    print('Rebuilding desktop icon cache…')
-    subprocess.call(['gtk-update-icon-cache', iconcachedir])
