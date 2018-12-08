d0285249d6bc1bff25ad8da94ab22fc7af9a9d79
Ian M. Jones <ian@ianmjones.com>
2018-12-08 00:34:50 +0000

    Updated application icon.

--- meson/post_install.py
+++ meson/post_install.py
@@ -4,7 +4,11 @@ import os
 import subprocess
 
 schemadir = os.path.join(os.environ['MESON_INSTALL_PREFIX'], 'share', 'glib-2.0', 'schemas')
+iconcachedir = os.path.join(os.environ['MESON_INSTALL_PREFIX'], 'share', 'icons', 'hicolor')
 
 if not os.environ.get('DESTDIR'):
     print('Compiling gsettings schemas...')
     subprocess.call(['glib-compile-schemas', schemadir])
+
+    print('Rebuilding desktop icon cache…')
+    subprocess.call(['gtk-update-icon-cache', iconcachedir])
